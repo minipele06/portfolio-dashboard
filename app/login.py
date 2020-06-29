3837bd9834cf02e3d160cbf125084a25f7361cf3
minipele06 <aja398@stern.nyu.edu>
2020-06-29 17:09:15 -0400

    Simplified login authorization

--- app/login.py
+++ app/login.py
@@ -6,16 +6,14 @@ def login_auth(username,password):
     with open(csv_filepath, "r") as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
-            if row["Username"] == str(username):
-                if row["Password"] == str(password):
-                    csv_filepath2 = os.path.join((os.path.dirname(__file__)),"..", "users", "active_user.csv")
-                    with open(csv_filepath2, "w") as csv_file:
-                        writer = csv.DictWriter(csv_file, fieldnames=["Username", "Email"])
-                        writer.writeheader()
-                        writer.writerow({"Username": username, "Email": row["Email"]})
-                    result = "Logged In"
-                    return result
-
+            if row["Username"] == str(username) and row["Password"] == str(password):
+                csv_filepath2 = os.path.join((os.path.dirname(__file__)),"..", "users", "active_user.csv")
+                with open(csv_filepath2, "w") as csv_file:
+                    writer = csv.DictWriter(csv_file, fieldnames=["Username", "Email"])
+                    writer.writeheader()
+                    writer.writerow({"Username": username, "Email": row["Email"]})
+                result = "Logged In"
+                return result
     result = "Username/Password Incorrect"
     return result
 
