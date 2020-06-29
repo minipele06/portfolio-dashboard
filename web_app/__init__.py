cb283b3b51bf52b5f873879874f5bdb743f30629
minipele06 <aja398@stern.nyu.edu>
2020-06-29 17:27:56 -0400

    Revert "unecessary file deletion"
    This reverts commit 9629051ae6b5c82eac8a1b6b0bd0d1e0d6a21c5a.

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -11,7 +11,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = SECRET_KEY
+
     app.register_blueprint(home_routes)
+    #app.register_blueprint(weather_routes)
+
     return app
 
 if __name__ == "__main__":
