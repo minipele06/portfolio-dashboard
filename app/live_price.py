7e6dff247a7192ecb8288d38ae66e80838426acf
minipele06 <aja398@stern.nyu.edu>
2020-06-29 19:34:57 -0400

    keyerror handling

--- app/live_price.py
+++ app/live_price.py
@@ -6,17 +6,21 @@ import pandas as pd
 import json
 
 def live_price(symbol,API_KEY):
-    if symbol.isalpha() == False:
+    try:
+        if symbol.isalpha() == False:
+            return "Invalid Stock Symbol, Try Again"
+        elif len(symbol) > 4:
+            return "Invalid Stock Symbol, Try Again"
+        elif symbol == "":
+            return "Invalid Stock Symbol, Try Again"
+        else:
+            url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
+            response = requests.get(url)
+            raw_data = json.loads(response.text)
+            return float(raw_data["Global Quote"]["05. price"])
+    except KeyError:
         return "Invalid Stock Symbol, Try Again"
-    elif len(symbol) > 4:
-        return "Invalid Stock Symbol, Try Again"
-    elif symbol == "":
-        return "Invalid Stock Symbol, Try Again"
-    else:
-        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
-        response = requests.get(url)
-        raw_data = json.loads(response.text)
-        return float(raw_data["Global Quote"]["05. price"])
+
 
 def transac_rec(symbol,price,shares,username,direction):
     csv_file_path = os.path.join((os.path.dirname(__file__)),"..", f"users/{username}", f"{username}.csv")
